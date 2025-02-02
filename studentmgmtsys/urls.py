d2979eeb4d01ffb5d39243ef916d26f1f5ca670e
raihan822 <raihansarkerxjc82@gmail.com>
2025-02-03 00:15:30 +0600

    raihan

--- studentmgmtsys/urls.py
+++ studentmgmtsys/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.views.generic import RedirectView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('students/', include('students.urls'))
+    path('students/', include('students.urls')),
+    path('', RedirectView.as_view(url='students/')),
 ]
